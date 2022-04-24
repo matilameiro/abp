65d7775aefd21418b4aa78863f530a555030e897
matilameiro <mati.lameiro@gmail.com>
2022-04-24 17:02:22 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { skaters_list } from './assets/ej_abp';
 
 function App() {
+  const [ value, setValue ] = useState('');
+  const [ skater, setSkater ] = useState(null);
+
+  const handleClick = ()=> {
+    const skaters = skaters_list;
+    console.log(value)
+    let skater = skaters.find( (skater) => skater.dni === Number(value));
+
+    console.log(skater);
+
+    if (skater) {
+      setSkater(skater);
+    } else {
+      setSkater(null)
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor:'black', height: '100vh'}}>
+      <h1 style={{color: 'white', margin: '1rem'}}>ABP</h1>
+      <h3 style={{ color: 'white', margin: '1rem' }}>Control de acceso</h3>
+      <div style={{marginTop: '1rem', display: 'flex', flexDirection:'column', alignItems: 'center'}}>
+        <label style={{ color: 'white' }}>Ingrese un num de doc: </label>
+        <input value={value} onChange={e => setValue(e.target.value)}></input>
+        <button onClick={handleClick} style={{marginTop:'1rem'}}>Buscar</button>
+      </div>
+
+      {
+        skater &&
+        <div >
+          <h3 style={{color: 'white'}}>El usuario {skater.name + ' ' + skater.lastname} puede ingresar.</h3>
+        </div>
+
+      }
     </div>
   );
 }
---
+++ src/assets/ej_abp.js
@@ -0,0 +1,147 @@
+export const skaters_list = [
+ {
+  "name": "Nombre 1",
+  "lastname": "Apellido 1",
+  "dni": 50000001
+ },
+ {
+  "name": "Nombre 2",
+  "lastname": "Apellido 2",
+  "dni": 50000002
+ },
+ {
+  "name": "Nombre 3",
+  "lastname": "Apellido 3",
+  "dni": 50000003
+ },
+ {
+  "name": "Nombre 4",
+  "lastname": "Apellido 4",
+  "dni": 50000004
+ },
+ {
+  "name": "Nombre 5",
+  "lastname": "Apellido 5",
+  "dni": 50000005
+ },
+ {
+  "name": "Nombre 6",
+  "lastname": "Apellido 6",
+  "dni": 50000006
+ },
+ {
+  "name": "Nombre 7",
+  "lastname": "Apellido 7",
+  "dni": 50000007
+ },
+ {
+  "name": "Nombre 8",
+  "lastname": "Apellido 8",
+  "dni": 50000008
+ },
+ {
+  "name": "Nombre 9",
+  "lastname": "Apellido 9",
+  "dni": 50000009
+ },
+ {
+  "name": "Nombre 10",
+  "lastname": "Apellido 10",
+  "dni": 50000010
+ },
+ {
+  "name": "Nombre 11",
+  "lastname": "Apellido 11",
+  "dni": 50000011
+ },
+ {
+  "name": "Nombre 12",
+  "lastname": "Apellido 12",
+  "dni": 50000012
+ },
+ {
+  "name": "Nombre 13",
+  "lastname": "Apellido 13",
+  "dni": 50000013
+ },
+ {
+  "name": "Nombre 14",
+  "lastname": "Apellido 14",
+  "dni": 50000014
+ },
+ {
+  "name": "Nombre 15",
+  "lastname": "Apellido 15",
+  "dni": 50000015
+ },
+ {
+  "name": "Nombre 16",
+  "lastname": "Apellido 16",
+  "dni": 50000016
+ },
+ {
+  "name": "Nombre 17",
+  "lastname": "Apellido 17",
+  "dni": 50000017
+ },
+ {
+  "name": "Nombre 18",
+  "lastname": "Apellido 18",
+  "dni": 50000018
+ },
+ {
+  "name": "Nombre 19",
+  "lastname": "Apellido 19",
+  "dni": 50000019
+ },
+ {
+  "name": "Nombre 20",
+  "lastname": "Apellido 20",
+  "dni": 50000020
+ },
+ {
+  "name": "Nombre 21",
+  "lastname": "Apellido 21",
+  "dni": 50000021
+ },
+ {
+  "name": "Nombre 22",
+  "lastname": "Apellido 22",
+  "dni": 50000022
+ },
+ {
+  "name": "Nombre 23",
+  "lastname": "Apellido 23",
+  "dni": 50000023
+ },
+ {
+  "name": "Nombre 24",
+  "lastname": "Apellido 24",
+  "dni": 50000024
+ },
+ {
+  "name": "Nombre 25",
+  "lastname": "Apellido 25",
+  "dni": 50000025
+ },
+ {
+  "name": "Nombre 26",
+  "lastname": "Apellido 26",
+  "dni": 50000026
+ },
+ {
+  "name": "Nombre 27",
+  "lastname": "Apellido 27",
+  "dni": 50000027
+ },
+ {
+  "name": "Nombre 28",
+  "lastname": "Apellido 28",
+  "dni": 50000028
+ },
+ {
+  "name": "Nombre 29",
+  "lastname": "Apellido 29",
+  "dni": 50000029
+ }
+]
